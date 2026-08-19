cf235c34361a8329816dc06261cb1b02c64d1bcb
MTVirux <joaofraga96@outlook.com>
2026-08-19 20:12:00 +0100

    Scaffold PolyglotTerror plugin project

---
+++ PolyglotTerror/Plugin.cs
@@ -0,0 +1,26 @@
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+
+namespace PolyglotTerror;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static ITargetManager TargetManager { get; private set; } = null!;
+    [PluginService] internal static IGameGui GameGui { get; private set; } = null!;
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+    public Plugin()
+    {
+        Log.Information("PolyglotTerror loaded.");
+    }
+
+    public void Dispose()
+    {
+    }
+}
